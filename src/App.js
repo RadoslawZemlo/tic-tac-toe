d6a3728b0d036325ab01e4b5b2d81e4f95f5abd1
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-04-12 16:03:07 +0200

    Setup react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic Tac Toe</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
