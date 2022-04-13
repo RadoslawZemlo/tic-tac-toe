3788cfbf7c1be53d82362939f187eb43b13f1d8d
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-04-13 12:11:50 +0200

    Add state indicating whose move is next and function enabling insertion of mark on board

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Board from "./components/Board";
 
 function App() {
   const [tiles, setTiles] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [x, setX] = useState(true);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tic Tac Toe</h1>
       </header>
-      <Board tiles={tiles} />
+      <Board tiles={tiles} setTiles={setTiles} x={x} setX={setX} />
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,35 @@
 import React from "react";
 
-const Board = ({ tiles }) => {
+const Board = ({ tiles, setTiles, x, setX }) => {
+  const handleClick = e => {
+    if (e.target.innerText !== "") return;
+
+    const updatedTiles = [...tiles];
+    const i = e.target.value;
+
+    if (x) {
+      updatedTiles[i] = "X";
+      setTiles(updatedTiles);
+    } else if (!x) {
+      updatedTiles[i] = "O";
+      setTiles(updatedTiles);
+    }
+
+    setX(!x);
+  };
+
   return (
     <div className="board">
-      {tiles.map(tile => (
-        <input type="submit" className="tile" value={tile} />
+      {tiles.map((tile, index) => (
+        <button
+          type="submit"
+          className="tile"
+          key={index}
+          value={index}
+          onClick={handleClick}
+        >
+          {tile}
+        </button>
       ))}
     </div>
   );
